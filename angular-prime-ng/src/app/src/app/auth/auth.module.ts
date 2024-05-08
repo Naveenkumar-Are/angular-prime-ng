eec7b9c70ae340707f8f77d38ff58d55b9f48b6f
Naveen kumar <naveenkumarare407@gmail.com>
2024-05-08 13:14:05 -0400

    V3-Module update

--- angular-prime-ng/src/app/src/app/auth/auth.module.ts
+++ angular-prime-ng/src/app/src/app/auth/auth.module.ts
@@ -9,10 +9,28 @@ import {StoreModule} from "@ngrx/store";
 import {CommonModule} from "@angular/common";
 import {RouterModule} from "@angular/router";
 import {SignupComponent} from "./signup/signup.component";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {InputTextModule} from "primeng/inputtext";
+import {ButtonModule} from "primeng/button";
+import {PasswordModule} from "primeng/password";
+import {CardModule} from "primeng/card";
+import {DropdownModule} from "primeng/dropdown";
+import {CalendarIcon} from "primeng/icons/calendar";
+import {CalendarModule} from "primeng/calendar";
 
 @NgModule({
   imports:[
     CommonModule,
+    FormsModule,
+    InputTextModule,
+    ButtonModule,
+    PasswordModule,
+    CardModule,
+    DropdownModule,
+    ReactiveFormsModule,
+
+    CalendarIcon,
+    CalendarModule,
     RouterModule.forChild([{path:'',component:LoginComponent}]),
     StoreModule.forFeature('auth',authReducer),
     EffectsModule.forFeature([AuthEffects])
